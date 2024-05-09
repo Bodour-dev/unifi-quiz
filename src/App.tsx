85089dfdab0c6d76ef209bcc8ed0d0b210222e32
Bodour Alshami <balshami@nbsventure.com>
2024-05-09 11:28:42 +0300

    add header & fix responsive issue + updated name card component to a more descriptive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import StolenBikesList from './pages/StolenBikesList';
+import React from "react";
+import "./App.css";
+import StolenBikesList from "./pages/StolenBikesList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-        <StolenBikesList />
+      <Navbar />
+      <StolenBikesList />
     </div>
   );
 }
--- src/components/BikeCard.tsx
+++ src/components/BikeCard.tsx
@@ -9,11 +9,51 @@ import Fade from "@mui/material/Fade";
 import { BikeProps } from "../GlobalTypes";
 import noBikeImg from "../assets/img/bike_placeholder.svg";
 import { UnixTimestampToDate } from "../utils/UnixTimestampToDate";
+import { useTheme } from "@mui/material/styles";
 
 const BikeCard: React.FC<{ bike: BikeProps }> = ({ bike }) => {
+  const theme = useTheme();
   const [expanded, setExpanded] = useState(true);
+
   const handleExpansion = () => {
     setExpanded((prevExpanded) => !prevExpanded);
+  };
+
+  const accordionSummaryStyles = {
+    borderBottom: "1px solid #ddd",
+    backgroundColor: expanded ? "#3498db" : "#333",
+    color: "#fff",
+  };
+
+  const accordionDetailsStyles = {
+    display: "flex !important",
+    justifyContent: "space-between",
+    alignItems: "center",
+    textAlign: "start",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+    },
+  };
+
+  const mediaStyles = {
+    height: "200px",
+    width: "250px",
+    flex: "0 0 30%",
+    background: "#f0f0f0",
+  };
+
+  const descriptionStyles = {
+    px: "1%",
+    flex: "0 0 38%",
+    wordBreak: "break-all",
+  };
+
+  const infoStyles = {
+    display: "flex",
+    alignItems: "baseline",
+    flexDirection: "column",
+    minWidth: "200px",
+    flex: "0 0 30%",
   };
 
   return (
@@ -33,58 +73,40 @@ const BikeCard: React.FC<{ bike: BikeProps }> = ({ bike }) => {
         expandIcon={<ExpandMoreIcon sx={{ color: "#fff" }} />}
         aria-controls="panel1-content"
         id="panel1-header"
-        sx={{
-          borderBottom: "1px solid #ddd",
-          backgroundColor: expanded ? "#3498db" : "#333",
-          color: "#fff",
-        }}
+        sx={accordionSummaryStyles}
       >
-        <Typography sx={{fontWeight:'300',letterSpacing:'1px'}}>{bike.title}</Typography>
+        <Typography sx={{ fontWeight: "300", letterSpacing: "1px" }}>
+          {bike.title}
+        </Typography>
       </AccordionSummary>
       <AccordionDetails
-        sx={{
-          display: "flex!important",
-          justifyContent: "space-between",
-          alignItems: "center",
-          textAlign: "start",
-        }}
+        sx={accordionDetailsStyles}
         className="accordion-details"
       >
         <CardMedia
           component="img"
-          sx={{ height: "200px", width: "250px", flex: "0 0 30%",background:'#f0f0f0' }}
-          image={bike.thumb ? bike.thumb : noBikeImg}
-          alt="Live from space album cover"
+          sx={mediaStyles}
+          image={bike.thumb || noBikeImg}
+          alt="Bike Thumbnail"
         />
         <Typography
           variant="subtitle1"
           color="text.secondary"
           component="div"
-          sx={{ px: "1%", flex: "0 0 38%" }}
+          sx={descriptionStyles}
         >
-          {`${bike.description ? bike.description : "-"}`}
+          {bike.description || "-"}
         </Typography>
 
-        <Typography
-          component="div"
-          sx={{
-            display: "flex",
-            alignItems: "baseline",
-            flexDirection: "column",
-            minWidth: "200px",
-            flex: "0 0 30%",
-          }}
-        >
-          <UnixTimestampToDate
-            timestamp={bike.date_stolen ? bike.date_stolen : 0}
-          />
+        <Typography component="div" sx={infoStyles}>
+          <UnixTimestampToDate timestamp={bike.date_stolen || 0} />
           <small>
             <strong>year:</strong>
-            {` ${bike.year ? bike.year : "-"}`}
+            {bike.year || "-"}
           </small>
           <small>
-            <strong style={{color:'red'}}>stolen_location:</strong>
-            {` ${bike.stolen_location ? bike.stolen_location : "-"}`}
+            <strong style={{ color: "red" }}>stolen_location:</strong>
+            {bike.stolen_location || "-"}
           </small>
         </Typography>
       </AccordionDetails>
